98ffe9d3b571a3010d59aa5a20adc24cdcfc4aae
Mertkaan <mertkaanmisir@gmail.com>
2026-09-16 10:58:55 +0200

    lommeregner

--- testprogram/c#/Program.cs
+++ testprogram/c#/Program.cs
@@ -1,3 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Skal lave en lommeregner
+/*
+float tal1;
+float tal2;
+char operatorTegn;
+float resultat = 0;
+Console.Write("Indtast første tal: ");
+tal1 = Convert.ToSingle(Console.ReadLine());
 
-Console.WriteLine("..!");
+Console.Write("Indtast en operator (+, -, *, /): ");
+operatorTegn = Convert.ToChar(Console.ReadLine()!);
+
+Console.Write("Indtast sidste tal: ");
+tal2 = Convert.ToSingle(Console.ReadLine());
+
+if (operatorTegn == '+'){
+    resultat = tal1 + tal2;
+}
+else if (operatorTegn == '-'){
+    resultat = tal1 - tal2;
+}
+else if (operatorTegn == '*'){
+    resultat = tal1 * tal2;
+}
+else if (operatorTegn == '/'){
+    resultat = tal1 / tal2;
+}
+
+Console.WriteLine("Resultatet er: " + resultat);
+*/
